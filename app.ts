3096c7d6e01a18f246f4acadeadaed6943355aa0
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-03-14 05:11:13 +0500

    Delete app.ts

--- app.ts
+++
@@ -1,8 +0,0 @@
-// 14 .Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
-let guestArr : string[]=["Noman","Ali raza","Mohsin","Fazal"]
-
-guestArr.map((item)=>(
-
- console.log(`Dear ${item} You Are Invited To a Dinner`)
-
-))
